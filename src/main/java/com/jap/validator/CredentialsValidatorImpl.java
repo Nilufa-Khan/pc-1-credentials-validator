004e0d40bdc5a5a004b350a5882cfc1a461e7d90
Nilufa Khan <khannilufa5@gmail.com>
2022-09-05 16:21:34 +0530

    Test case passed

--- src/main/java/com/jap/validator/CredentialsValidatorImpl.java
+++ src/main/java/com/jap/validator/CredentialsValidatorImpl.java
@@ -2,7 +2,15 @@ package com.jap.validator;
 
 public class CredentialsValidatorImpl {
     public String validateUser(String name, String password){
-        return "";
+        CredentialsValidator c1 = (o1, o2) -> {
+            if (o1.equals("admin") && o2.equals("1234")) {
+                return "valid user";
+            }else {
+                return "invalid user";
+            }
+        };
+
+        return c1.validator(name,password);
     }
 
 
